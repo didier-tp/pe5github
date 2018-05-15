dcb6a98dd71bad36fefe5278717691c438667396
Baro Abdoulaye <alcatrazroba@hotmail.com>
2018-05-15 09:57:01 +0200

    version3

--- pe5/src/main/java/pe5/AppCommun.java
+++ pe5/src/main/java/pe5/AppCommun.java
@@ -4,7 +4,7 @@ public class AppCommun {
 
 	public static void main(String[] args) {
 		System.out.println("app commun rim");
-		System.out.println("app commun titi");
+		System.out.println("app commun titi ***");
 
 	}
 
