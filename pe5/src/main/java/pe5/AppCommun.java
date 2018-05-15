1d12b699f197a97e51040c578fa0021426318744
Baro Abdoulaye <alcatrazroba@hotmail.com>
2018-05-15 11:22:19 +0200

    coorection de bug en urgence master

--- pe5/src/main/java/pe5/AppCommun.java
+++ pe5/src/main/java/pe5/AppCommun.java
@@ -5,6 +5,7 @@ public class AppCommun {
 	public static void main(String[] args) {
 		System.out.println("app commun rim frikha");
 		System.out.println("app commun titi ***");
+		System.out.println("coorection de bug en urgence ");
 
 	}
 
