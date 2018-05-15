d3ed480b0ad3a332240b40a753dfa65d366ef668
rim frikha <rimfrikha89@hotmail.fr>
2018-05-15 10:29:34 +0200

    version ajout frikha

--- pe5/src/main/java/pe5/AppCommun.java
+++ pe5/src/main/java/pe5/AppCommun.java
@@ -3,7 +3,7 @@ package pe5;
 public class AppCommun {
 
 	public static void main(String[] args) {
-		System.out.println("app commun rim");
+		System.out.println("app commun rim frikha");
 		System.out.println("app commun titi ***");
 
 	}
