7be71304643088962c9ed2074c0993d98a204604
Baro Abdoulaye <alcatrazroba@hotmail.com>
2018-05-15 11:12:04 +0200

    3 eme version dev

--- pe5/src/main/java/pe5/AppAbdou.java
+++ pe5/src/main/java/pe5/AppAbdou.java
@@ -5,6 +5,8 @@ public class AppAbdou {
 	public static void main(String[] args) {
 		System.out.println("app abdou");
 		System.out.println("yeah yeahh");
+		int a = 2;
+		System.out.println("a= + 2");
 
 
 	}
