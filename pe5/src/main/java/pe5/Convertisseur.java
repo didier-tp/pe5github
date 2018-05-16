0984dedd9b1ca0c6ab60003c0583636730fc1993
rim frikha <rimfrikha89@hotmail.fr>
2018-05-16 10:28:20 +0200

    erreur volontaire pour tester jenkins

--- pe5/src/main/java/pe5/Convertisseur.java
+++ pe5/src/main/java/pe5/Convertisseur.java
@@ -2,7 +2,7 @@ package pe5;
 
 public class Convertisseur {
 	public double euroToFranc(double montantEnEuro) {
-		return  montantEnEuro* 6.55957;
+		return  montantEnEuro* 16.55957;
 	}
 	
 	public double francToEuro (double montantEnFranc){
